82e5a7748b830069997d1e1a050027839816b686
ddimasss@mail.ru <Serialnomberr3>
2019-05-24 22:13:18 +0200

    fix meta model

--- meta_api/admin.py
+++ meta_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bars
 
+admin.site.register(Bars)
 # Register your models here.
--- meta_api/models.py
+++ meta_api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-class Ticks(models.Model):
-    name = models.CharField(max_length=20)
+class Bars(models.Model):
+    pair = models.CharField(max_length=20)
+    timeframe = models.CharField(max_length=5)
+    datetime = models.DateTimeField()
+    high = models.FloatField()
+    low = models.FloatField()
+    open = models.FloatField()
+    close = models.FloatField()
 
+    def __str__(self):
+        return self.pair + ' ' + self.timeframe + ' ' + str(self.datetime)
 # Create your models here.
